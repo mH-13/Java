8abb5b8d5d8239457527e24106448ad12ea86439
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-01-14 23:49:26 +0600

    first commit

---
+++ DataType.java
@@ -0,0 +1,9 @@
+public class DataType {
+  public static void main(String[] args) {
+      int age = 25;
+      double salary = 50000.50;
+      char grade = 'A';
+      boolean isActive = true;
+      System.out.println("Age: " + age + ", Salary: " + salary + ", Grade: " + grade + ", Active: " + isActive);
+  }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+      System.out.println("Hello, World!");
+  }
+}
+//public: Access modifier.
+//class: Defines a class.
+//main: Entry point of the application.
